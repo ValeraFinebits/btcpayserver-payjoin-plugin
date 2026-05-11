ccf14399ccf8a12dccff34db3b1203c1e7b58b98
Valera <50830352+ValeraFinebits@users.noreply.github.com>
2026-05-11 19:27:45 +0300

    test: Re-enable skipped Payjoin concurrency tests

--- BTCPayServer.Plugins.Payjoin.IntegrationTests/PayjoinPluginConcurrencyIntegrationTests.cs
+++ BTCPayServer.Plugins.Payjoin.IntegrationTests/PayjoinPluginConcurrencyIntegrationTests.cs
@@ -177,7 +177,7 @@ public class PayjoinPluginConcurrencyIntegrationTests : UnitTestBase
         await PayjoinReceiverTestHelper.AssertReceiverSessionEventuallyRemovedAsync(tester, failedInvoiceId, cts.Token).ConfigureAwait(true);
     }
 
-    [Theory (Skip = "Temporarily skipped will re-enable later") ]
+    [Theory]
     [InlineData(1, 4)]
     [InlineData(2, 4)]
     [InlineData(4, 4)]
@@ -266,7 +266,7 @@ public class PayjoinPluginConcurrencyIntegrationTests : UnitTestBase
         Assert.DoesNotContain(sessionStore.GetSessions(), session => invoiceContexts.Any(invoiceContext => invoiceContext.InvoiceId == session.InvoiceId));
     }
 
-    [Theory(Skip = "Temporarily skipped will re-enable later")]
+    [Theory]
     [InlineData(4, 8)]
     [InlineData(4, 16)]
     [InlineData(8, 16)]
