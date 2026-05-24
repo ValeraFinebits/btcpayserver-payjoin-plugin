9a9a4f206ea4932ddcf787280b551669de5cdb8e
Valera <50830352+ValeraFinebits@users.noreply.github.com>
2026-05-24 14:18:36 +0300

    fix: fix after rebase

--- BTCPayServer.Plugins.Payjoin.IntegrationTests/PayjoinPluginConcurrencyIntegrationTests.cs
+++ BTCPayServer.Plugins.Payjoin.IntegrationTests/PayjoinPluginConcurrencyIntegrationTests.cs
@@ -2,7 +2,6 @@ using BTCPayServer.Plugins.Payjoin.IntegrationTests.TestUtils;
 using BTCPayServer.Plugins.Payjoin.Services;
 using BTCPayServer.Tests;
 using NBitpayClient;
-using NBXplorer;
 using Xunit;
 using Xunit.Abstractions;
 
