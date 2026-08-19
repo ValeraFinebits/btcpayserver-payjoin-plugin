d03e04a51a01ceed9a5a8747bf78babde1883d92
chavic <dev.chavic@gmail.com>
2026-08-19 22:16:11 +0200

    Answer the remaining review findings on the sender

--- BTCPayServer.Plugins.Payjoin/Migrations/PayjoinPluginDbContextModelSnapshot.cs
+++ BTCPayServer.Plugins.Payjoin/Migrations/PayjoinPluginDbContextModelSnapshot.cs
@@ -296,6 +296,10 @@ namespace BTCPayServer.Plugins.Payjoin.Migrations
                         .HasMaxLength(64)
                         .HasColumnType("character varying(64)");
 
+                    b.Property<string>("CoinReservationTransactionId")
+                        .HasMaxLength(64)
+                        .HasColumnType("character varying(64)");
+
                     b.Property<DateTimeOffset>("CreatedAt")
                         .HasColumnType("timestamp with time zone");
 
@@ -342,6 +346,11 @@ namespace BTCPayServer.Plugins.Payjoin.Migrations
 
                     b.HasKey("SenderSessionId")
                         .HasName("PK_SenderSessions");
+
+                    b.HasIndex("Bip21")
+                        .IsUnique()
+                        .HasDatabaseName("IX_SenderSessions_Bip21_Live")
+                        .HasFilter("\"Status\" IN (0, 4)");
 
                     b.HasIndex("OriginalTransactionId")
                         .HasDatabaseName("IX_SenderSessions_OriginalTransactionId");
